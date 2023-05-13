ac40484d673a77691bf613571c70259df4ef63dc
RedGreenBlue09 <redgreenblue@outlook.com.vn>
2023-05-14 02:34:14 +0700

    Alternative fix for Dualboot.cmd
    I cannot confirm the mountvol method so I went for this one.

--- Files/PostInstall/Dualboot.cmd
+++ Files/PostInstall/Dualboot.cmd
@@ -1,32 +1,32 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-:MOSPath
-set "MainOS="
-echo.
-set /p MainOS=%ESC%[92mEnter MainOS Path: %ESC%[93m
-if not defined MainOS (
-	echo  %ESC%[91mNot a valid MainOS partition.
-	goto MOSPath
-)
-if not exist "%MainOS%\EFIESP" (
-	echo  %ESC%[91mNot a valid MainOS partition.
-	goto MOSPath
-)
-if not exist "%MainOS%\Data" (
-	echo  %ESC%[91mNot a valid MainOS partition.
-	goto MOSPath
+:: Get EFIESP infos
+
+for /f "delims=" %%a in ('fsutil reparsepoint query %SYSTEMDRIVE%\EFIESP') do (
+	echo %%a| find "Substitute Name:">nul
+	if "!Errorlevel!" EQU "0" (
+		for /f "tokens=2 delims=:" %%b in ("%%a") do (
+			set "VolumeName=%%b"
+			set "Uuid=!VolumeName:*Volume=!"
+			set "Uuid=!Uuid:~0,-1!"
+			for /f %%p in ('Powershell -C "(Get-Partition | ? { $_.Guid -eq '!Uuid!'}).DiskNumber"') do set "DiskNumber=%%p"
+			for /f %%p in ('Powershell -C "(Get-Partition | ? { $_.Guid -eq '!Uuid!'}).PartitionNumber"') do set "PartitionNumber=%%p"
+			set "Uuid="
+		)
+	)
 )
 
-::DualBoot
-mountvol X: /S
+:: Dualboot
 
 md Temp\
-echo>Temp\diskpart1.txt sel vol=X:
+echo>Temp\diskpart1.txt sel dis !DiskNumber!
+echo>>Temp\diskpart1.txt sel par !PartitionNumber!
 echo>>Temp\diskpart1.txt set id=ebd0a0a2-b9e5-4433-87c0-68b6b72699c7
 diskpart /s Temp\diskpart1.txt
 rd /s /q Temp\
 
-bcdedit /store "%MainOS%\EFIESP\EFI\Microsoft\Boot\BCD" /set "{bootmgr}" "timeout" "5"
+bcdedit /store "%SYSTEMDRIVE%\EFIESP\EFI\Microsoft\Boot\BCD" /set "{bootmgr}" "timeout" "5"
 
 echo.
 echo Done!
